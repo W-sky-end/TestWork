86a2687cac918a3ce5cbc2ce02adad362588c3ea
Vadim <aonyko@gmail.com>
2024-12-26 15:50:11 +0200

    Com Init

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<String> students = new ArrayList<>();
+
+        System.out.println("Enter the name students(if enough enter \"Stop\"): ");
+
+        while (true){
+            String name = sc.next();
+            if (name.equalsIgnoreCase("Stop")){
+                break;
+            }
+            students.add(name);
+        }
+        sc.close();
+        System.out.println(students);
+
+
+    }
+}
